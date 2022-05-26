919d2fcb94669f26659e78b24f33df91205ddebb
Bruno Berger <bruberger@t-online.de>
2022-05-26 17:36:06 +0200

    base cam-shake on hit

--- Assets/Scripts/PlayerHit.cs
+++ Assets/Scripts/PlayerHit.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MilkShake;
 
 public class PlayerHit : MonoBehaviour
 {
     [SerializeField] GameState gameState;
     [SerializeField] RhythmController rhythmController;
+    [SerializeField] ShakePreset shakePreset;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Obstacle")
         {
+            //shakeInstance.Start(0.2f);
+            Shaker.ShakeAll(shakePreset);
             if (gameState.speed > 1)
                 rhythmController.getSober();
         }
