c852d357db43bb601ae79a20c3df27f915126e7b
Bruno Berger <bruberger@t-online.de>
2022-06-01 12:25:44 +0200

    fov effect to drink

--- Assets/Rhythm/RhythmController.cs
+++ Assets/Rhythm/RhythmController.cs
@@ -17,12 +17,18 @@ public class RhythmController : MonoBehaviour
     int beatPos = 0;
     bool nextTact = false;
     float slowestTactSpeed;
+
+    Camera cam;
+    float normalFOV = 60f;
+
     // Start is called before the first frame update
     void Start()
     {
         nextTact = true;
         beatPos = 0;
         slowestTactSpeed = 1 / gameState.speed;
+        cam = Camera.main;
+        cam.fieldOfView = normalFOV;
     }
 
     // Update is called once per frame
@@ -69,6 +75,7 @@ public class RhythmController : MonoBehaviour
         gameState.speed += 0.25f;
         gameState.beerCounter -= 1;
         jumpPreset.jumpDuration = 1 / gameState.speed / 2;
+        StartCoroutine(speedEffect());
     }
 
     public void getSober()
@@ -106,5 +113,41 @@ public class RhythmController : MonoBehaviour
             beatPos++;
 
         nextTact = true;
+    }
+
+    IEnumerator speedEffect()
+    {
+        float fov_transition = 0f;
+        float effectFOV = normalFOV + 10f;
+        //Vector3 offset = new Vector3(0, 0, 0.2f);
+        //Vector3 relativeCamPos = transform.position - cam.transform.position;
+
+        while (fov_transition <= 1)
+        {
+            cam.fieldOfView = Mathf.Lerp(normalFOV, effectFOV, fov_transition);
+            //cam.transform.position = Vector3.Lerp(
+            //    cam.transform.position, 
+            //    cam.transform.position + offset, 
+            //    fov_transition
+            //);
+
+            fov_transition += Time.deltaTime * 6;
+            yield return null;
+        }
+        fov_transition = 0f;
+        while (fov_transition <= 1)
+        {
+            cam.fieldOfView = Mathf.Lerp(effectFOV, normalFOV, fov_transition);
+            //cam.transform.position = Vector3.Lerp(
+            //    cam.transform.position - offset, 
+            //    cam.transform.position, 
+            //    fov_transition
+            //);
+
+            fov_transition += Time.deltaTime * 1f;
+            yield return null;
+        }
+        //cam.fieldOfView = normalFOV;
+        //cam.transform.position = transform.position + relativeCamPos;
     }
 }
